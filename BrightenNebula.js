2fce2758660482931cc5fd4534eb59702e8d8688
Juergen <juergen@ehnes.eu>
2021-01-06 20:18:11 +0100

    executing starnet on an image

--- BrightenNebula.js
+++ BrightenNebula.js
@@ -26,55 +26,173 @@
 
 #define DEBUG true
 
+function BrightenNebula(view) {
+
+   var starnet = new StarNet();
+   starnet.mask = false;
+
+   var view2 = new View(view);
+
+   result = starnet.executeOn(view2);
+
+}
 function BrightenNebulaDialog() {
 // Add all properties and methods of the core Dialog object to this object.
-this.__base__ = Dialog;
-this.__base__();
+   this.__base__ = Dialog;
+   this.__base__();
 
-var dlg = this;
+   var dlg = this;
 
-//var labelWidth1 = this.font.width( "Overlay Mode:" + 'T' );
-var running = false;
-this.prvImgW = null;
-var sliderMaxValue = 360;
-var sliderMinWidth = 256;
+   //var labelWidth1 = this.font.width( "Overlay Mode:" + 'T' );
+   var running = false;
+   this.prvImgW = null;
+   var sliderMaxValue = 360;
+   var sliderMinWidth = 256;
 
-// ------------------------------------------------------------------------
-// Top help panel
-// ------------------------------------------------------------------------
+   // ------------------------------------------------------------------------
+   // Top help panel
+   // ------------------------------------------------------------------------
 
-this.helpLabel = new Label( this );
-with ( this.helpLabel )
-{
-frameStyle = FrameStyle_Box;
-margin = 4;
-readOnly = true;
-wordWrapping = true;
-useRichText = true;
+   this.helpLabel = new Label( this );
+   with ( this.helpLabel )
+   {
+      frameStyle = FrameStyle_Box;
+      margin = 4;
+      readOnly = true;
+      wordWrapping = true;
+      useRichText = true;
 
-text = "<p><b>" + TITLE + " v" + VERSION + "</b> &mdash; " +
-"Brighten Nebula Utility.</p>" +
-"<p>Brightens a nebula in an image by negative multiplication." +
-"<p>Author: Juergen Ehnes </p>" +
-"<p></p>";
-}
+      text = "<p><b>" + TITLE + " v" + VERSION + "</b> &mdash; " +
+      "Brighten Nebula Utility.</p>" +
+      "<p>Brightens a nebula in an image by negative multiplication." +
+      "<p>Author: Juergen Ehnes </p>" +
+      "<p></p>";
+    }
 
-this.imageS
+   this.targetImage_Sizer = new HorizontalSizer;
+   this.targetImage_Sizer.spacing = 4;
 
+   this.targetImage_Label = new Label(this);
+   this.targetImage_Label.text = "Target image: ";
+   this.targetImage_Label.textAlignment = TextAlign_Right|TextAlign_VertCenter;
 
-this.windowTitle = TITLE + " Script";
+   this.targetImage_ViewList = new ViewList(this);
+   this.targetImage_ViewList.minWidth = 360;
+   this.targetImage_ViewList.getAll(); // include main views as well as previews
+   this.targetImage_ViewList.currentView = data.targetView;
+   this.targetImage_ViewList.toolTip = this.targetImage_Label.toolTip = "Select the image that will be used to generate the mask.";
+
+   this.targetImage_ViewList.onViewSelected = function(view) {
+      data.targetView = view;
+   };
+
+   this.targetImage_Sizer.add(this.targetImage_Label);
+   this.targetImage_Sizer.add(this.targetImage_ViewList, 100);
+
+   this.buttons_Sizer = new HorizontalSizer;
+   this.buttons_Sizer.spacing = 6;
+
+   this.newInstance_Button = new ToolButton(this);
+   this.newInstance_Button.icon = new Bitmap( ":/process-interface/new-instance.png" );
+   this.newInstance_Button.toolTip = "New Instance";
+   this.newInstance_Button.onMousePress = function()
+   {
+      this.hasFocus = true;
+      exportParameters();
+      this.pushed = false;
+      this.dialog.newInstance();
+   };
+
+   this.ok_Button = new PushButton(this);
+   this.ok_Button.text = " OK ";
+#ifneq __PI_PLATFORM__ MACOSX
+    this.ok_Button.icon = new Bitmap( ":/icons/ok.png" );
+#endif
+
+   this.ok_Button.onClick = function() {
+      this.dialog.ok();
+   };
+
+   this.cancel_Button = new PushButton(this);
+   this.cancel_Button.text = " Cancel ";
+#ifneq __PI_PLATFORM__ MACOSX
+    this.cancel_Button.icon = new Bitmap( ":/icons/cancel.png" );
+#endif
+
+   this.cancel_Button.onClick = function() {
+      this.dialog.cancel();
+   };
+
+   /* this.buttons_Sizer.add(this.newInstance_Button); */
+   this.buttons_Sizer.add(this.newInstance_Button);
+   this.buttons_Sizer.addStretch();
+   this.buttons_Sizer.add(this.ok_Button);
+   this.buttons_Sizer.add(this.cancel_Button);
+
+   this.sizer = new VerticalSizer;
+   this.sizer.margin = 6;
+   this.sizer.spacing = 6;
+   this.sizer.add(this.helpLabel);
+   this.sizer.addSpacing(4);
+   this.sizer.add(this.targetImage_Sizer);
+   this.sizer.add(this.buttons_Sizer);
+
+   this.windowTitle = TITLE + " Script";
+   this.adjustToContents();
+   this.setFixedSize();
+
 }
 
 BrightenNebulaDialog.prototype = new Dialog;
 
 function main() {
-Console.writeln(TITLE + " started")
-if (!DEBUG)
-Console.hide();
-var dialog = new BrightenNebulaDialog();
-var retVal = dialog.execute();
+   Console.writeln(TITLE + " started")
+   if (!DEBUG) {
+      Console.hide();
+   }
 
-Console.writeln(TITLE + " ended")
+    if (!data.targetView) {
+      (new MessageBox("There is no active image window!",
+         TITLE, StdIcon_Error, StdButton_Ok)).execute();
+      return;
+   }
+
+   var dialog = new BrightenNebulaDialog();
+
+   for (;;) {
+      if (!dialog.execute()) {
+         break;
+      }
+
+      // A view must be selected.
+      if (data.targetView.isNull) {
+         (new MessageBox("You must select a view to apply this script.",
+               TITLE, StdIcon_Error, StdButton_Ok)).execute();
+         continue;
+      }
+
+      // Must be a color image?
+      if (data.targetView.image.numberOfChannels != 3) {
+         (new MessageBox("You must supply an RGB color image.",
+               TITLE, StdIcon_Error, StdButton_Ok)).execute();
+         continue;
+      }
+
+      console.abortEnabled = true;
+      console.show();
+
+      var start = new Date;
+
+      data.targetView.beginProcess(UndoFlag_NoSwapFile);
+      BrightenNebula(data.targetView);
+      data.targetView.endProcess();
+
+      var timeNeeded = ((new Date).getTime() - start.getTime() / 1000);
+
+      console.writeln(format("<end><cbr>BrightenNebula: %.2f s", timeNeeded));
+      break;
+   }
+   Console.writeln(TITLE + " ended")
 
 }
 
